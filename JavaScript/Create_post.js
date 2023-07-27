b00dd63fe4d1e5286355a86c1281210ac1f77e50
Divyesh-Sar2003 <119989934+Divyesh-Sar2003@users.noreply.github.com>
2023-07-27 22:40:59 +0530

    Update js file

--- JavaScript/Create_post.js
+++ JavaScript/Create_post.js
@@ -1,26 +1,48 @@
-function myfunction()
-{
-    window.location.href="Home.html";
-}
-// 
-function open_file() {
-    document.getElementById('input_file').click();
-}
-const input_file= document.getElementById("input_file");
-const imgPreview = document.getElementById("img_preview");
-input_file.addEventListener("change", function () {
-    getImgData();
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { getDatabase ,ref , set } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+   apiKey: "AIzaSyDzuQMqCSk21coM76TukGRWnxr2_Tqf4tw",
+   authDomain: "techx-project.firebaseapp.com",
+   databaseURL: "https://techx-project-default-rtdb.firebaseio.com",
+   projectId: "techx-project",
+   storageBucket: "techx-project.appspot.com",
+   messagingSenderId: "941951611823",
+   appId: "1:941951611823:web:d7441a1f7a9610d5a8355a",
+   measurementId: "G-BSE9VLMKCR"
+};
+//Initialize Firebase
+let firebase= initializeApp(firebaseConfig);
+const db = getDatabase();
+
+const fileInput = document.querySelector('input');
+// Listen for the change event so we can capture the file
+fileInput.addEventListener('change',  (e) => {
+    // Get a reference to the file
+    const file = e.target.files[0];
+    // Encode the file using the FileReader API
+    const reader = new FileReader();
+    reader.onloadend = () => {
+    var endcode = reader.result;
+   //  get data from localstorage
+    var photo = localStorage.getItem('photoURL');
+    var user = localStorage.getItem('Name');
+    var uemail = localStorage.getItem('emailID');
+    //create an object of image
+   let post =
+    {
+      UserName:user,
+      UserEmail:uemail,
+      Image :endcode,
+      Userpic:photo
+   } 
+   // set object into rtdb
+   set(ref(db,user),{
+      post
+  });
+    // Logs data:<type>;base64,wL2dvYWwgbW9yZ...
+    };
+    reader.readAsDataURL(file);
 });
-function getImgData() 
-{
-    const files = input_file.files[0];
-    if (files) {
-        const fileReader = new FileReader();
-        fileReader.readAsDataURL(files);
-        fileReader.addEventListener("load", function () 
-        {
-            imgPreview.style.display = "block";
-            imgPreview.innerHTML = '<img src="' + this.result + '" />';
-        });    
-    }
-}
